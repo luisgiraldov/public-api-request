94ce95a9464c7398c518baafbad9b30811f4edb3
Luis Giraldo <lugirva@gmail.com>
2020-03-28 18:17:56 -0400

    Added getUsers function
    Added appendUsers function

--- js/script.js
+++ js/script.js
@@ -17,6 +17,30 @@
         });     
     }
 
+    function appendUsers(users){
+        const gallery = document.getElementById("gallery");
+        let usersHTML = "";
+        users.results.map( user => {
+            const card = `<div class="card">
+                            <div class="card-img-container">
+                                <img class="card-img" src="${user.picture.large}" alt="profile picture">
+                            </div>
+                            <div class="card-info-container">
+                                <h3 id="${user.name.first}${user.name.last}" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+                                <p class="card-text">${user.email}</p>
+                                <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+                            </div>
+                        </div>`;
+            
+            usersHTML += card;
+            console.log(user);
+        });
+        gallery.innerHTML = usersHTML;
+    }
 
+    
+    getUsers().then( users => {
+        appendUsers(users);
+    });
     
 })();
